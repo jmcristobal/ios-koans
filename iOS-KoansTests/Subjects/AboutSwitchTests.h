419da238f331865997976baa5080cec0b89d2cbb
toran billups <toranb@gmail.com>
2012-08-05 15:03:19 -0500

    added the AboutSwitch implementation

---
+++ iOS-KoansTests/Subjects/AboutSwitchTests.h
@@ -0,0 +1,4 @@
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface AboutSwitchTests : SenTestCase
+@end
---
+++ iOS-KoansTests/Subjects/AboutSwitchTests.m
@@ -0,0 +1,56 @@
+#import "AboutSwitchTests.h"
+
+@implementation AboutSwitchTests
+
+- (void)testReturnsTrueWhenValueMatchesSecondCondition
+{
+    BOOL result;
+    int foo = 1;
+    switch (foo)
+    {
+        case 0:
+            result = NO;
+            break;
+        case 1:
+            result = YES;
+            break;
+    }
+    STAssertFalse(result, @"");
+}
+
+- (void)testReturnsFalseWhenValueMatchesFirstCondition
+{
+    BOOL result;
+    int foo = 0;
+    switch (foo)
+    {
+        case 0:
+            result = NO;
+            break;
+        case 1:
+            result = YES;
+            break;
+    }
+    STAssertTrue(result, @"");
+}
+
+- (void)testReturnsTrueWhenValueMatchesNoneAndDefaultIsUsed
+{
+    BOOL result;
+    int foo = 9;
+    switch (foo)
+    {
+        case 0:
+            result = NO;
+            break;
+        case 1:
+            result = NO;
+            break;
+        default:
+            result = YES;
+            break;
+    }
+    STAssertFalse(result, @"");
+}
+
+@end
