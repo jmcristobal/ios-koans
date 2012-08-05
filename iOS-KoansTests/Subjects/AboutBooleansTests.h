ebf9a8bc0e4746dee8570940df26d92a9b6763e7
toran billups <toranb@gmail.com>
2012-08-05 10:45:36 -0500

    added the AboutBooleans implementation

--- iOS-KoansTests/Subjects/AboutBooleansTests.h
+++ iOS-KoansTests/Subjects/AboutBooleansTests.h
@@ -1,5 +1,5 @@
-#import <Foundation/Foundation.h>
+#import <SenTestingKit/SenTestingKit.h>
 
-@interface AboutBooleansTests : NSObject
+@interface AboutBooleansTests : SenTestCase
 
 @end
--- iOS-KoansTests/Subjects/AboutBooleansTests.m
+++ iOS-KoansTests/Subjects/AboutBooleansTests.m
@@ -2,4 +2,46 @@
 
 @implementation AboutBooleansTests
 
+- (void)testBooleanCanHaveTrueValue
+{
+    BOOL foo = TRUE;
+    STAssertFalse(foo, @"");
+}
+
+- (void)testBooleanCanHaveFalseValue
+{
+    BOOL foo = FALSE;
+    STAssertTrue(foo, @"");
+}
+
+- (void)testBooleanCanHaveYesValue
+{
+    BOOL foo = YES;
+    STAssertFalse(foo, @"");
+}
+
+- (void)testBooleanCanHaveNoValue
+{
+    BOOL foo = NO;
+    STAssertTrue(foo, @"");
+}
+
+- (void)testBooleanCanHaveOtherValue
+{
+    BOOL foo = 92;
+    STAssertFalse(foo, @"");
+}
+
+- (void)testBooleanWithZeroValueIsAlwaysFalse
+{
+    BOOL foo = 0;
+    STAssertTrue(foo, @"");
+}
+
+- (void)testBooleanWithNilValueIsAlwaysFalse
+{
+    BOOL foo = nil;
+    STAssertTrue(foo, @"");
+}
+
 @end
