ec9cd5c3088d64eb3d2ab920e615a995e9ce98db
Jose Manuel Cristobal Cristobal <jmcristobal@incita.eu>
2013-01-21 15:27:04 +0100

    [TEST] Numbers Koan passed

--- iOS-KoansTests/Subjects/AboutNumbersTests.m
+++ iOS-KoansTests/Subjects/AboutNumbersTests.m
@@ -5,52 +5,52 @@
 - (void)testSignedIntegerCanHavePositiveValue
 {
     int foo = 24;
-    STAssertEquals(foo, nil, @"");
+    STAssertEquals(foo, 24, @"");
 }
 
 - (void)testSignedIntegerCanAlsoHaveNegativeValue
 {
     int foo = -24;
-    STAssertEquals(foo, nil, @"");
+    STAssertEquals(foo, -24, @"");
 }
 
 - (void)testSignedIntegerIsTheDefault
 {
     int foo = 24;
     signed int bar = 24;
-    STAssertEquals(foo, nil, @"");
+    STAssertEquals(foo, bar, @"");
 }
 
 - (void)testNSIntegerCanHavePositiveValue
 {
     NSInteger foo = 24;
-    STAssertEquals(foo, nil, @"");
+    STAssertEquals(foo, 24, @"");
 }
 
 - (void)testNSIntegerIsSignedIntegerWithPositiveValue
 {
     int foo = 24;
     NSInteger bar = 24;
-    STAssertEquals(foo, nil, @"");
+    STAssertEquals(foo, bar, @"");
 }
 
 - (void)testNSUIntegerIsUnsignedIntegerWithPositiveValue
 {
     unsigned int foo = 24;
     NSUInteger bar = 24;
-    STAssertEquals(foo, nil, @"");
+    STAssertEquals(foo, bar, @"");
 }
 
 - (void)testFloatContainsMoreInformationThanInteger
 {
     float foo = 24.44444;
-    STAssertEquals(foo, nil, @"");
+    STAssertEquals(foo, 24.44444f, @"");
 }
 
 - (void)testDoubleContainsMoreInformationThanFloat
 {
     double foo = 24.444444444444444444444448;
-    STAssertEquals(foo, nil, @"");
+    STAssertEquals(foo, 24.444444444444444444444448, @"");
 }
 
 @end
